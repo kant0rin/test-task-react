0b06b683c02db4f680a5e4b0b2e05f0b72f49f95
kant0rin <kantorin.ilya@yandex.ru>
2023-08-03 14:07:25 +1000

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Main from "./section/Main.tsx";
+import {useState} from 'react';
+import Header from "./components/Header/Header.tsx";
+import PostList from "./components/PostList/PostList.tsx";
+import Modal from './components/Modal/Modal.tsx';
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [currentPost, setCurrentPost] = useState({title: '', text: '', image: '', image2x: ''})
+  const [isModalActive, setIsModalActive] = useState<boolean>(false)
+
+
   return (
     <>
-      <Main/>
+      <Header setQuery={setSearchQuery} query={searchQuery}/>
+      <Modal setCurrentPost={setCurrentPost} currentPost={currentPost} setIsActive={setIsModalActive} isActive={isModalActive}/>
+      <PostList query={searchQuery} setCurrentPost={setCurrentPost} setModalActive={setIsModalActive}/>
     </>
   )
 }
