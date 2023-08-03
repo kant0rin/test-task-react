1f8618c84e2d8d452a0376298be56941ba7e217b
kant0rin <kantorin.ilya@yandex.ru>
2023-08-03 13:42:48 +1000

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import {useState} from 'react';
-import Header from "./components/Header/Header.tsx";
-import PostList from "./components/PostList/PostList.tsx";
-import Modal from './components/Modal/Modal.tsx';
+import Main from "./section/Main.tsx";
 
 function App() {
-
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const [currentPost, setCurrentPost] = useState({title: '', text: '', image: '', image2x: ''})
-  const [isModalActive, setIsModalActive] = useState<boolean>(false)
-
-
   return (
     <>
-      <Header setQuery={setSearchQuery} query={searchQuery}/>
-      <Modal setCurrentPost={setCurrentPost} currentPost={currentPost} setIsActive={setIsModalActive} isActive={isModalActive}/>
-      <PostList query={searchQuery} setCurrentPost={setCurrentPost} setModalActive={setIsModalActive}/>
+      <Main/>
     </>
   )
 }
--- src/components/ui/icons/SearchIcon.tsx
+++ src/components/ui/icons/SearchIcon.tsx
@@ -1,5 +1,3 @@
-
-
 const SearchIcon = () => {
   return (
     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
